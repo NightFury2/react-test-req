da336142e143059bf5e9f7b246b9db4da0a5eebb
nightfury <lunvvova42@gmail.com>
2017-02-27 14:33:47 +0300

    fix this proxy server

--- src/server.js
+++ src/server.js
@@ -51,18 +51,6 @@ server.on('upgrade', (req, socket, head) => {
 });
 
 // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
-proxy.on('error', (error, req, res) => {
-  let json;
-  if (error.code !== 'ECONNRESET') {
-    console.error('proxy error', error);
-  }
-  if (!res.headersSent) {
-    res.writeHead(500, {'content-type': 'application/json'});
-  }
-
-  json = {error: 'proxy_error', reason: error.message};
-  res.end(JSON.stringify(json));
-});
 
 app.use((req, res) => {
   if (__DEVELOPMENT__) {
